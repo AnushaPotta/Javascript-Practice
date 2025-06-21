a47143fa4243071a45dc2921386487d3d3a4dc6e
Anusha Potta <anusha.potta@gmail.com>
2025-06-20 19:18:43 -0700

    shape program with classes

---
+++ shape.js
@@ -0,0 +1,32 @@
+//You're going to build a base class called Shape and extend it to create a Circle class.
+
+//✅ Expected Behavior:
+
+//const circle = new Circle(5);
+//console.log(circle.area());         // 👉 78.54 (approx)
+//console.log(circle.describe());     // 👉 A circle with radius 5
+
+class Shape {
+  describe() {
+    return "This is a shape";
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
+  area() {
+    const A = Math.PI * this.radius * this.radius;
+    return `Area of the circle is ${A}`;
+  }
+
+  describe() {
+    return `A circle with radius ${this.radius}`;
+  }
+}
+
+const circle1 = new Circle(5);
+console.log(circle1.area());
+console.log(circle1.describe());
