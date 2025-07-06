59ee6c9338c41f1bafc5169d5ab65e9f8d5e627e
Anusha Potta <anusha.potta@gmail.com>
2025-07-06 11:40:33 -0700

    DOM15 adding pwd strength bar

--- DOM15/script.js
+++ DOM15/script.js
@@ -1,19 +1,35 @@
 const inputPwd = document.getElementById("inputPwd");
 const pwdStrength = document.getElementById("pwdStrength");
+const strengthBar = document.getElementById("strengthBar");
 
 inputPwd.addEventListener("input", (event) => {
   const value = event.target.value;
-  if (value.length < 8) {
+
+  let Strength = 0;
+
+  if (value.length >= 8) Strength++;
+  if (/[0-9]/.test(value)) Strength++;
+  if (/[!@#$%^&*()_+{}]/.test(value)) Strength++;
+  if (Strength === 0) {
     pwdStrength.style.color = "red";
+    strengthBar.style.width = "0%";
     pwdStrength.textContent = "Strength: Too weak";
-  } else if (!/[0-9]/.test(value)) {
+  } else if (Strength === 1) {
     pwdStrength.style.color = "red";
-    pwdStrength.textContent = "Strength: Include atleast one number";
-  } else if (!/[!@#$%^&*()_+{}]/.test(value)) {
+
+    pwdStrength.textContent = "Strength: Weak";
+    strengthBar.style.width = "33%";
+  } else if (Strength === 2) {
     pwdStrength.style.color = "red";
-    pwdStrength.textContent = "Strength: Include atleast one special character";
-  } else {
+    strengthBar.style.width = "66%";
+
+    pwdStrength.textContent = "Strength: Medium";
+    strengthBar.style.backgroundColor = "orange";
+  } else if (Strength === 3) {
     pwdStrength.style.color = "green";
+    strengthBar.style.width = "100%";
+
     pwdStrength.textContent = "Strength: Strong";
+    strengthBar.style.backgroundColor = "green";
   }
 });
