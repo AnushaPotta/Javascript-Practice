3996896de59b1ecca0014aa2c2be51fe49c8b93d
Anusha Potta <anusha.potta@gmail.com>
2025-07-27 20:09:40 -0700

    DOM25 note app

---
+++ DOM25/script.js
@@ -0,0 +1,53 @@
+const titleInput = document.getElementById("titleInput");
+
+const notes = document.getElementById("notes");
+
+const saveBtn = document.getElementById("saveBtn");
+const notesList = document.getElementById("notesList");
+const noteForm = document.getElementById("noteForm");
+
+window.addEventListener("DOMContentLoaded", displayNotes());
+
+saveBtn.addEventListener("click", () => {
+  const title = titleInput.value.trim();
+  const note = notes.value.trim();
+
+  if (title && note) {
+    try {
+      const existingNotes = JSON.parse(localStorage.getItem("note")) || [];
+
+      const newNote = {
+        title: title,
+        note: note,
+      };
+      existingNotes.push(newNote);
+
+      localStorage.setItem("note", JSON.stringify(existingNotes));
+      noteForm.reset();
+      displayNotes();
+    } catch (error) {
+      console.error("Error saving note", error);
+      alert("Something went wrong while saving your notes");
+    }
+  }
+});
+
+function displayNotes() {
+  try {
+    const savedNote = localStorage.getItem("note");
+    const parsedNote = JSON.parse(savedNote);
+    if (parsedNote) {
+      notesList.innerHTML = "";
+      parsedNote.forEach((n) => {
+        const li = document.createElement("li");
+        li.textContent = `${n.title} - ${n.note}`;
+        notesList.appendChild(li);
+      });
+    } else {
+      notesList.innerHTML = "No saved notes";
+    }
+  } catch (error) {
+    console.error("Error loading notes:", error);
+    alert("couldn't load notes. Data may be corrupted");
+  }
+}
