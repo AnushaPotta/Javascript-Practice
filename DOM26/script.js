7942a0c1cd2009c233aa639c15759c99f45aa59b
Anusha Potta <anusha.potta@gmail.com>
2025-08-01 12:14:11 -0700

    DOM26 Task tracker adding delete button

--- DOM26/script.js
+++ DOM26/script.js
@@ -11,40 +11,55 @@ window.addEventListener("DOMContentLoaded", displayTasks());
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const task = taskName.value.trim();
-  const due = dueDate.value.trim();
-  const existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  try {
+    const task = taskName.value.trim();
+    const due = dueDate.value.trim();
+    const existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  const data = {
-    task: task,
-    due: due,
-  };
-  existingTasks.push(data);
-  localStorage.setItem("tasks", JSON.stringify(existingTasks));
-  console.log(existingTasks);
-  taskForm.reset();
-  displayTasks();
+    const data = {
+      task: task,
+      due: due,
+    };
+    existingTasks.push(data);
+    localStorage.setItem("tasks", JSON.stringify(existingTasks));
+    console.log(existingTasks);
+    taskForm.reset();
+    displayTasks();
+  } catch (error) {
+    console.log("can not add the task", error);
+  }
 });
 
 function displayTasks() {
-  const savedTasks = localStorage.getItem("tasks");
-  console.log(savedTasks);
-  const parsedTasks = JSON.parse(savedTasks);
-  console.log(parsedTasks);
-  taskList.textContent = "";
+  try {
+    const savedTasks = localStorage.getItem("tasks");
+    console.log(savedTasks);
+    const parsedTasks = JSON.parse(savedTasks);
+    console.log(parsedTasks);
+    taskList.textContent = "";
 
-  parsedTasks.forEach((t) => {
-    const task = `${t.task} - due by ${t.due}`;
-    const li = document.createElement("li");
-    li.textContent = task;
-    const btn = document.createElement("button");
-    btn.style.marginLeft = "10px";
-    btn.textContent = "Done";
-    btn.addEventListener("click", () => {
-      li.style.textDecoration = "line-through";
-      li.style.color = "green";
+    parsedTasks.forEach((t) => {
+      const task = `${t.task} - due by ${t.due}`;
+      const li = document.createElement("li");
+      li.textContent = task;
+      const btn = document.createElement("button");
+      btn.style.marginLeft = "10px";
+      btn.textContent = "Done";
+      const delBtn = document.createElement("button");
+      delBtn.textContent = "Delete";
+      delBtn.style.marginLeft = "10px";
+      btn.addEventListener("click", () => {
+        li.style.textDecoration = "line-through";
+        li.style.color = "green";
+      });
+      delBtn.addEventListener("click", () => {
+        li.remove();
+      });
+      li.appendChild(btn);
+      li.appendChild(delBtn);
+      taskList.appendChild(li);
     });
-    li.appendChild(btn);
-    taskList.appendChild(li);
-  });
+  } catch (error) {
+    console.log("Can not display tasks", error);
+  }
 }
